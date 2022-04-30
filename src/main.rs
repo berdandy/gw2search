157c9b3320e53ea6cfa6fabac9bb61a979d8c0d9
Andy Berdan <andy@berdan.ca>
2022-04-30 10:31:45 -0400

    Working gui implementation. Some warts: initial download is blocking and feedback is 100% on console, so invisible to casual users

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod request;
 mod tests;
 
 use iced::{
-    button, scrollable, text_input, Align, Button, Column, Container, Element, Length,
-    ProgressBar, Radio, Row, Rule, Sandbox, Scrollable, Settings, Space, Text, TextInput,
+    scrollable, text_input, pick_list, Align, Column, Element,
+	PickList, Row, Rule, Sandbox, Scrollable, Settings, Text, TextInput,
 };
 
 use config::CONFIG;
@@ -30,9 +30,42 @@ pub fn main() -> iced::Result {
     Gw2Search::run(Settings::default())
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum SearchMode {
+	Item,
+	Skill,
+	Trait,
+}
+impl Default for SearchMode {
+    fn default() -> Self { SearchMode::Item }
+}
+impl SearchMode {
+    const ALL: [SearchMode; 3] = [
+		SearchMode::Item,
+		SearchMode::Skill,
+		SearchMode::Trait,
+    ];
+}
+impl std::fmt::Display for SearchMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                SearchMode::Item => "Item",
+                SearchMode::Skill => "Skill",
+                SearchMode::Trait => "Trait",
+            }
+        )
+    }
+}
+
+#[derive(Default)]
 struct Gw2Search {
     scroll: scrollable::State,
     input: text_input::State,
+	pick_list: pick_list::State<SearchMode>,
+	search_mode: SearchMode,
     search_term: String,
 	results: Vec<String>,
 }
@@ -41,20 +74,14 @@ struct Gw2Search {
 enum Message {
 	Search,
 	SearchTermChanged(String),
-	// SearchComplete(Vec<String>),
-	// Scrolled(usize, f32),
+	SearchModeSelected(SearchMode),
 }
 
 impl Sandbox for Gw2Search {
     type Message = Message;
 
 	fn new() -> Self {
-		Gw2Search {
-			scroll: scrollable::State::new(),
-			input: text_input::State::new(),
-			search_term: String::new(), 
-			results: Vec::new(),
-		}
+		Self::default()
 	}
 
 	fn title(&self) -> String {
@@ -67,10 +94,23 @@ impl Sandbox for Gw2Search {
 			Message::SearchTermChanged(term) => {
 				self.search_term = term;
 			}
+			Message::SearchModeSelected(search_mode) => {
+				self.search_mode = search_mode;
+
+				if ! self.search_term.is_empty() {
+					match search_api(self.search_mode, self.search_term.clone()) {
+						Ok(results) => {
+							self.results = results;
+						}
+						Err(error) => panic!("Problem with search {:?}", error)
+					}
+				}
+			}
 			Message::Search => {
-				println!("TODO: SEARCH {}", self.search_term);
-				match search_api(self.search_term.clone()) {
-					Ok(()) => println!("done"),
+				match search_api(self.search_mode, self.search_term.clone()) {
+					Ok(results) => {
+						self.results = results;
+					}
 					Err(error) => panic!("Problem with search {:?}", error)
 				}
 			}
@@ -79,148 +119,140 @@ impl Sandbox for Gw2Search {
 
 	fn view(&mut self) -> Element<Self::Message> {
 
-		let results_col = Column::new()
-			.spacing(20)
-			.padding(20)
-			.align_items(Align::Start);
-			//.push(self.results.iter().map(|&result| Text::new(result)));
-
-		for result in &self.results {
-			results_col.push(Text::new(result));
-		}
+		let results = self.results.iter().fold(
+            Column::new().spacing(10).push(Text::new("Results:")),
+            |column: Column<Message>, result| {
+                column.push(
+					Text::new(result)
+                )
+            },
+        );
 
 		Column::new()
             .spacing(20)
 			.padding(20)
 			.align_items(Align::Center)
-            .push(Text::new("Gw2Search"))
+            .push(Text::new("gw2search").size(32))
             .push(Rule::horizontal(20))
             .push(
-				TextInput::new(
-					&mut self.input,
-					"Search Term",
-					&self.search_term,
-					Message::SearchTermChanged
-				).on_submit(Message::Search)
+				Row::new()
+				.push(TextInput::new(
+						&mut self.input,
+						"Search Term",
+						&self.search_term,
+						Message::SearchTermChanged
+					).on_submit(Message::Search)
+				)
+				.push(
+					PickList::new(
+						&mut self.pick_list,
+						&SearchMode::ALL[..],
+						Some(self.search_mode),
+						Message::SearchModeSelected)
+				)
 			)
+			.push(Text::new("input search term and hit [ENTER] to search"))
 			.push(
 				Scrollable::new(&mut self.scroll)
 					.padding(40)
-					.push(results_col)
+					.push(results)
 			)
 			.into()
 		}
 }
 
 #[tokio::main]
-async fn search_api(search_term: String) -> Result<(), Box<dyn std::error::Error>> {
-	if CONFIG.skill {
-		debug!("Loading skills");
-		let skills: Vec<api::Skill> = request::get_data(&CONFIG.skills_file, || async {
-			let api_skills: Vec<api::ApiSkill> =
-				request::request_paginated("skills", &CONFIG.lang).await?;
-			Ok(api_skills
-				.into_iter()
-				.map(|api_skill| api::Skill::from(api_skill))
-				.collect())
-		})
-		.await?;
-		debug!(
-			"Loaded {} items stored at '{}'",
-			skills.len(),
-			CONFIG.skills_file.display()
-		);
-
-		let skill_name = &search_term;
-		let results: Vec<_> = skills
-			.iter()
-			.filter_map(|skill| match &skill.name.to_ascii_lowercase().contains(&skill_name.to_ascii_lowercase()) {
-				true => Some(skill),
-				false => None
+async fn search_api(search_mode: SearchMode, search_term: String) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+	match search_mode {
+		SearchMode::Skill => {
+			debug!("Loading skills");
+			let skills: Vec<api::Skill> = request::get_data(&CONFIG.skills_file, || async {
+				let api_skills: Vec<api::ApiSkill> =
+					request::request_paginated("skills", &CONFIG.lang).await?;
+				Ok(api_skills
+					.into_iter()
+					.map(|api_skill| api::Skill::from(api_skill))
+					.collect())
 			})
-			.collect();
-
-		println!("Results found: {}", results.len());
-
-		for result in results {
-			println!("{} : {}",
-				result.id,
-				result.name
+			.await?;
+			debug!(
+				"Loaded {} skills stored at '{}'",
+				skills.len(),
+				CONFIG.skills_file.display()
 			);
+
+			let skill_name = &search_term;
+			let results: Vec<_> = skills
+				.iter()
+				.filter_map(|skill| match &skill.name.to_ascii_lowercase().contains(&skill_name.to_ascii_lowercase()) {
+					true => Some(skill),
+					false => None
+				})
+				.map(|result| format!("{}: {}", result.id, result.name))
+				.collect::<Vec<String>>();
+
+			return Ok(results);
+
 		}
+		SearchMode::Trait => {
+			debug!("Loading traits");
 
-	} else if CONFIG.r#trait {
-		debug!("Loading traits");
-
-		let traits: Vec<api::Trait> = request::get_data(&CONFIG.traits_file, || async {
-			let api_traits: Vec<api::ApiTrait> =
-				request::request_paginated("traits", &CONFIG.lang).await?;
-			Ok(api_traits
-				.into_iter()
-				.map(|api_trait| api::Trait::from(api_trait))
-				.collect())
-		})
-		.await?;
-		debug!(
-			"Loaded {} items stored at '{}'",
-			traits.len(),
-			CONFIG.traits_file.display()
-		);
-
-		let trait_name = &search_term;
-		let results: Vec<_> = traits
-			.iter()
-			.filter_map(|r#trait| match &r#trait.name.to_ascii_lowercase().contains(&trait_name.to_ascii_lowercase()) {
-				true => Some(r#trait),
-				false => None
+			let traits: Vec<api::Trait> = request::get_data(&CONFIG.traits_file, || async {
+				let api_traits: Vec<api::ApiTrait> =
+					request::request_paginated("traits", &CONFIG.lang).await?;
+				Ok(api_traits
+					.into_iter()
+					.map(|api_trait| api::Trait::from(api_trait))
+					.collect())
 			})
-			.collect();
-
-		println!("Results found: {}", results.len());
-
-		for result in results {
-			println!("{} : {}",
-				result.id,
-				result.name
+			.await?;
+			debug!(
+				"Loaded {} traits stored at '{}'",
+				traits.len(),
+				CONFIG.traits_file.display()
 			);
+
+			let trait_name = &search_term;
+			let results: Vec<_> = traits
+				.iter()
+				.filter_map(|r#trait| match &r#trait.name.to_ascii_lowercase().contains(&trait_name.to_ascii_lowercase()) {
+					true => Some(r#trait),
+					false => None
+				})
+				.map(|result| format!("{}: {}", result.id, result.name))
+				.collect::<Vec<String>>();
+
+			return Ok(results);
+
 		}
-
-	} else if CONFIG.item {
-		debug!("Loading items");
-		let items: Vec<api::Item> = request::get_data(&CONFIG.items_file, || async {
-			let api_items: Vec<api::ApiItem> =
-				request::request_paginated("items", &CONFIG.lang).await?;
-			Ok(api_items
-				.into_iter()
-				.map(|api_item| api::Item::from(api_item))
-				.collect())
-		})
-		.await?;
-		debug!(
-			"Loaded {} items stored at '{}'",
-			items.len(),
-			CONFIG.items_file.display()
-		);
-
-		let item_name = &search_term;
-		let results: Vec<_> = items
-			.iter()
-			.filter_map(|item| match &item.name.to_ascii_lowercase().contains(&item_name.to_ascii_lowercase()) {
-				true => Some(item),
-				false => None
+		SearchMode::Item => {
+			debug!("Loading items");
+			let items: Vec<api::Item> = request::get_data(&CONFIG.items_file, || async {
+				let api_items: Vec<api::ApiItem> =
+					request::request_paginated("items", &CONFIG.lang).await?;
+				Ok(api_items
+					.into_iter()
+					.map(|api_item| api::Item::from(api_item))
+					.collect())
 			})
-			.collect();
-
-		println!("Results found: {}", results.len());
-
-		for result in results {
-			println!("{} : {} [{:?}]",
-				result.id,
-				result.name,
-				result.item_type
+			.await?;
+			debug!(
+				"Loaded {} items stored at '{}'",
+				items.len(),
+				CONFIG.items_file.display()
 			);
+
+			let item_name = &search_term;
+			let results: Vec<_> = items
+				.iter()
+				.filter_map(|item| match &item.name.to_ascii_lowercase().contains(&item_name.to_ascii_lowercase()) {
+					true => Some(item),
+					false => None
+				})
+				.map(|result| format!("{}: {}", result.id, result.name))
+				.collect::<Vec<String>>();
+
+			return Ok(results);
 		}
 	}
-
-	Ok(())
 }
